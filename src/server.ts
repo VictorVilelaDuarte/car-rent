7f237e213ba96a0de9d597252ba1f296c2c5bc84
Victor Vilela Duarte <victorvduarte@gmail.com>
2022-03-31 20:42:07 -0300

    post categories route

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3333)
+
+app.use('/categories', categoriesRoutes);
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Server is live!' })
